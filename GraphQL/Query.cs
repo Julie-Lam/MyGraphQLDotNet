87ce875b0d790784e860cdbe2f9ea6ca49c2ba8e
Julie <lam.julie@hotmail.com>
2026-03-03 00:01:49 +1100

    - Installed hotchocolate - DI graphQL - DI QueryType - Set the default url to graphQL endpoint

---
+++ GraphQL/Query.cs
@@ -0,0 +1,30 @@
+﻿using MyGraphQLDotNet.Models;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace MyGraphQLDotNet.GraphQL
+{
+    public class Query
+    {
+        public List<Book> Books(string nameContains = "")
+        {
+            var books = ReadAllBooks();
+            if (string.IsNullOrWhiteSpace(nameContains))
+            {
+                return books;
+            }
+
+
+            return books.Where(b => b.Name.Contains(nameContains)).ToList();
+
+
+        }
+
+        private List<Book> ReadAllBooks()
+        {
+            string fileName = "Database/books.json";
+            string jsonString = File.ReadAllText(fileName);
+            return JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() } })!;
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace MyGraphQLDotNet.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public int Pages { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
